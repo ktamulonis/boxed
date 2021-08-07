8bba8afed2b78e16c00b8dfc1052820c2d0859b7
Kurt Tamulonis <kurttamulonis@gmail.com>
2021-08-06 20:16:23 -0400

    builds game fields

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ApplicationRecord
+  belongs_to :player
+  belongs_to :opponent, class_name: 'Player'
   validates_presence_of :player_id, :opponent_id
+  has_many :boxes
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,6 @@ class Player < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :games
+  has_many :boxes
 end
