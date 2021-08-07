f659df7b2f4f6b94832e52fd648ccc0d2f029e9c
Kurt Tamulonis <kurttamulonis@gmail.com>
2021-08-07 00:55:35 -0400

    turbo stream boxes

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,3 +1,10 @@
 class Box < ApplicationRecord
+  include ActionView::RecordIdentifier
+
+  belongs_to :player
   belongs_to :game
+
+  after_create_commit do
+    broadcast_append_to game, target: dom_id(player), partial: 'games/box', locals: {box: self}
+  end
 end
